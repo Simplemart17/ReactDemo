2348ec87eed54df303af28f8338586eafd559068
Martins Aloba <aloba.martins@gmail.com>
2019-03-10 07:57:50 +0100

    learn reactjs

---
+++ src/robots.js
@@ -0,0 +1,52 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Jane Doe',
+    email: 'jane.doe@mail.com'
+  },
+  {
+    id: 2,
+    name: 'Janie John',
+    email: 'janiejohn@mail.com'
+  },
+  {
+    id: 3,
+    name: 'Janet Dray',
+    email: 'janet_dray@mail.com'
+  },
+  {
+    id: 4,
+    name: 'Yang David',
+    email: 'david-yang@mail.com'
+  },
+  {
+    id: 5,
+    name: 'Nick Casco',
+    email: 'nick@mail.com'
+  },
+  {
+    id: 6,
+    name: 'Yank Carr',
+    email: 'ycarr@mail.com'
+  },
+  {
+    id: 7,
+    name: 'kick Carry',
+    email: 'kcarry@mail.com'
+  },
+  {
+    id: 8,
+    name: 'Annick Saco',
+    email: 'saconick@mail.com'
+  },
+  {
+    id: 9,
+    name: 'Yannick Carrasco',
+    email: 'yannick@mail.com'
+  },
+  {
+    id: 10,
+    name: 'Yannick Rasco',
+    email: 'yick@gmail.com'
+  }
+];
